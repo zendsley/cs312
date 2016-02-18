b47389040cc58c41924a283bb30d8e1f335e685c
Zack Endsley <xzen21@gmail.com>
2016-02-18 17:40:47 -0600

    Initial Commit

---
+++ zends_matAvg/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  zends_matAvg
+//
+//  Created by Zackary Endsley on 2/18/16.
+//  Copyright © 2016 Zackary Endsley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
